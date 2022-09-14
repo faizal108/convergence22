7f0396b404768e2c003d28aacd33514a13472f4d
Faizal <faizal108@gmail.com>
2022-09-14 15:00:38 +0530

    event-viewer added

---
+++ assets/js/viewer.js
@@ -0,0 +1,6 @@
+$(".events .event-card").click(function () {
+     $(".viewer-wrapper").addClass("open");
+ });
+$(".viewer-wrapper .btn-close").click(function () {
+     $(".viewer-wrapper").removeClass("open");
+ });
