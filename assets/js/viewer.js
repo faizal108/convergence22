e10d799d084cfca58b3d8ec15dda55384dc6da2a
Jay <Jaykhatri225@gmail.com>
2022-09-17 09:50:56 +0530

    infocrats events added

--- assets/js/viewer.js
+++ assets/js/viewer.js
@@ -1,6 +1,62 @@
-$(".events .event-card").click(function () {
-     $(".viewer-wrapper").addClass("open");
+$("#1").click(function () {
+     $("#view1").addClass("open");
  });
-$(".viewer-wrapper .btn-close").click(function () {
-     $(".viewer-wrapper").removeClass("open");
+$("#view1 .btn-close").click(function () {
+     $("#view1").removeClass("open");
  });
+
+$("#2").click(function () {
+    $("#view2").addClass("open");
+});
+$("#view2 .btn-close").click(function () {
+    $("#view2").removeClass("open");
+});
+
+$("#3").click(function () {
+    $("#view3").addClass("open");
+});
+$("#view3 .btn-close").click(function () {
+    $("#view3").removeClass("open");
+});
+
+$("#4").click(function () {
+    $("#view4").addClass("open");
+});
+$("#view4 .btn-close").click(function () {
+    $("#view4").removeClass("open");
+});
+
+$("#5").click(function () {
+    $("#view5").addClass("open");
+});
+$("#view5 .btn-close").click(function () {
+    $("#view5").removeClass("open");
+});
+
+$("#6").click(function () {
+    $("#view6").addClass("open");
+});
+$("#view6 .btn-close").click(function () {
+    $("#view6").removeClass("open");
+});
+
+$("#7").click(function () {
+    $("#view7").addClass("open");
+});
+$("#view7 .btn-close").click(function () {
+    $("#view7").removeClass("open");
+});
+
+$("#8").click(function () {
+    $("#view8").addClass("open");
+});
+$("#view8 .btn-close").click(function () {
+    $("#view8").removeClass("open");
+});
+
+$("#9").click(function () {
+    $("#view9").addClass("open");
+});
+$("#view9 .btn-close").click(function () {
+    $("#view9").removeClass("open");
+});
